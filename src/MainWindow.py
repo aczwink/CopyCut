057758888398b36103af8904ca358198c5aff90c
Amir Czwink <amir130@hotmail.de>
2017-10-25 11:06:35 +0200

    added license header to source code files

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -1,3 +1,21 @@
+__license__ = u"""
+Copyright (c) 2017 Amir Czwink (amir130@hotmail.de)
+
+This file is part of CopyCut.
+
+CopyCut is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+CopyCut is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with CopyCut.  If not, see <https://www.gnu.org/licenses/>.
+"""
 import os;
 from PyQt5.QtCore import *;
 from PyQt5.QtGui import *;
